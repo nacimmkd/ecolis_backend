9403701461e8c3ed97221a4abfc71df659794580
nacimmkd <nacimmakedhi@gmail.com>
2026-04-04 07:06:59 +0200

    UPDATE: restructure application.yaml and jwtCnfig

--- src/main/java/com/deliveryplatform/auth/jwt/JwtConfig.java
+++ src/main/java/com/deliveryplatform/auth/jwt/JwtConfig.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "spring.jwt")
+@ConfigurationProperties(prefix = "jwt")
 @Data
 public class JwtConfig {
     private String secret;
