3cb79a45459b980dc428489a19254820e369af01
nacimmkd <nacimmakedhi@gmail.com>
2026-04-05 20:21:16 +0200

    FIX: fix address test controller || used only for testing

--- src/main/java/com/deliveryplatform/addresses/GeocodingController.java
+++ src/main/java/com/deliveryplatform/addresses/GeocodingController.java
@@ -5,8 +5,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+// NOTE : Controller just for testing
+
 @RestController
-@RequestMapping("/api/geocoding")
+@RequestMapping("/api/v1/geocoding")
 @RequiredArgsConstructor
 public class GeocodingController {
 
