7698f65d5922e2ad3df287eb262104fd29cad730
nacimmkd <nacimmakedhi@gmail.com>
2026-05-14 05:04:58 +0200

    UPDATE: updated dtos

--- src/main/java/com/deliveryplatform/images/Image.java
+++ src/main/java/com/deliveryplatform/images/Image.java
@@ -32,9 +32,6 @@ public class Image {
     @Builder.Default
     private boolean confirmed = false;
 
-    @Transient
-    private String url;
-
     @Column(name = "created_at")
     @Builder.Default
     private OffsetDateTime createdAt = OffsetDateTime.now();
